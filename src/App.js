cf075562f86047f4d9c3eeedda7034ddc45a0f11
DhanashreeNirmale <dhanashreenirmale@gmail.com>
2026-02-22 12:37:00 +0530

    Updated code for sakhi

--- src/App.js
+++ src/App.js
@@ -3,21 +3,25 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex justify-center">
+      <div className="border-2 border-blue-900 rounded-xl shadow-2xl m-4 w-1/4">
+        <h1 className="bg-blue-900 text-center text-xl text-white rounded-t-md p-2">
+          Calculator App
+        </h1>
+
+        <div className="flex p-2">
+          <input
+            type="text"
+            className="border border-blue-800 p-3 m-2 text-center w-1/2 rounded-md"
+            placeholder="num1"
+          />
+          <input
+            type="text"
+            className="border border-blue-900 p-3 m-2 text-center w-1/2 rounded-md"
+            placeholder="num2"
+          />
+        </div>
+      </div>
     </div>
   );
 }
